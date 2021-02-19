e3ca395bda5643c88c86abbd0a960f3598fe80b4
Juan Pablo Calao <jcalaop@gmail.com>
2021-02-19 09:41:15 -0500

    move types definition to shared directory

---
+++ src/Shared/types/ToDo.types.ts
@@ -0,0 +1,11 @@
+export interface IToDo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+export interface IToDoPrimitives {
+  title: string
+  description?: string
+  completed?: boolean
+}
